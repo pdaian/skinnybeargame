0200bccc2eea88f8468a6db93b86e853e1db42e6
Phil D <phil@linux.com>
2024-10-13 18:46:34 -0400

    1080p borderless window default

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ class App:
         self.done = False
         pg.display.init()
         self.screen = pg.display.set_mode(RES, pg.HIDDEN, vsync=0)
-        self.window = Window("sb and the power of <3", resizable=True)
+        self.window = Window("sb and the power of <3", size=RES, resizable=True, borderless=True)
         self.renderer = Renderer(self.window)
         self.clock = pg.time.Clock()
         self.time = 0
